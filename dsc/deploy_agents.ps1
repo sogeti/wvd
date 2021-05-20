8f667b1e66670da60f11f6ab9a8819a88376395e
Peter Rombouts <prombouts@gmail.com>
2021-05-20 18:36:24 +0200

    Temporary DSC script

---
+++ dsc/deploy_agents.ps1
@@ -0,0 +1,20 @@
+﻿param ($VHDLocations, $InstallSourceFSLogix, $InstallSourceABR)
+
+New-Item -Path "C:\" -Name "Install" -ItemType "directory"
+
+if (-not ([string]::IsNullOrEmpty($InstallSourceFsLogix)))
+{
+    New-Item -Path "HKLM:\SOFTWARE" -Name "FSLogix"
+    New-Item -Path "HKLM:\SOFTWARE\FSLogix" -Name "Profiles"
+    New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "Enabled" -Value "1" -PropertyType "Dword" -Force
+    New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "VHDLocations" -Value $VHDLocations -PropertyType "MultiString" -Force
+    New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "DeleteLocalProfileWhenVHDShouldApply" -Value "1" -PropertyType "Dword" -Force
+    Invoke-WebRequest -Uri $InstallSourceFSLogix -OutFile "C:\Install\FSLogixAppsSetup.exe"
+    Start-Process -Wait -FilePath "C:\Install\FSLogixAppsSetup.exe" -ArgumentList "/install /quiet" -PassThru
+}
+
+if (-not ([string]::IsNullOrEmpty($InstallSourceABR)))
+{
+    Invoke-WebRequest -Uri $InstallSourceABR -OutFile "C:\Install\abr_7_ws.msi"
+    Start-Process -Wait -FilePath "msiexec" -ArgumentList "/i C:\Install\abr_7_ws.msi /q" -PassThru
+}
